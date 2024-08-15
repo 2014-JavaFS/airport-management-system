9f9e82b4a10909b93cefd308583675cbef54ad3c
Charles Jester <charles.jester@revature.com>
2024-08-15 16:56:36 -0400

    included the expected react endpoint on aws

--- src/main/java/com/revature/ams/Flight/FlightController.java
+++ src/main/java/com/revature/ams/Flight/FlightController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "http://ec2-13-59-42-43.us-east-2.compute.amazonaws.com:5005/"})
 @RestController
 @RequestMapping("/flights")
 public class FlightController {
